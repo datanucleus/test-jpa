cb75d205af05cc1ab41cebc10c2647d98b1b5a41
andyjefferson <andy@datanucleus.org>
2017-07-15 09:36:00 +0100

    minor tweaks, JPA version

--- src/main/java/mydomain/model/Person.java
+++ src/main/java/mydomain/model/Person.java
@@ -16,13 +16,13 @@ public class Person
         this.name = name;
     }
 
-    public String getName()
-    {
-        return name;
-    }
-
     public Long getId()
     {
         return id;
+    }
+
+    public String getName()
+    {
+        return name;
     }
 }
